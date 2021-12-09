7fe2a4e35e98f3c708134262a4a46f71070ad6d9
Ferdinand Firman <ferdinand040799@gmail.com>
2021-12-09 22:54:08 +0700

    include

---
+++ include/footer.php
@@ -0,0 +1,12 @@
+<div class="footer fixed-bottom">
+		<p style="font-size: 10px"> 
+			<a class="fa-2x fab fa-youtube ml-3 mt-2"> GKI Penginjil Sukabumi</a>
+			<a class="fa-2x fas fa-envelope ml-3 mt-2"> gki_penginjilsmi@gmail.com</a>
+			<a class="fa-2x fab fa-instagram ml-3 mt-2"> @gki_penginjilsmi</a>
+			<a class="fa-2x fab fa-facebook-square ml-3 mt-2"> GKI Penginjil Sukabumi</a>
+			<a class="fa-2x fab fa-whatsapp ml-3 mt-2"> +6289252911068</a>
+
+		</p>
+	</div>
+</body>
+</html> 
---
+++ jadwal.php
@@ -0,0 +1,64 @@
+<?php
+	require 'include/header.php';
+  require 'include/navbar.php';
+?>
+
+	<div class ="card" style="text-align: center">
+    <div class="card-body">
+      <div class="row">
+        <div class="col-md-4 col-sm-2">
+          <h3><b>KEBAKTIAN UMUM</b></h3>
+          <p>KEBAKTIAN UMUM 1<br>
+          Setiap hari Minggu pukul 07.00 WIB</p>
+          <p>KEBAKTIAN UMUM 2<br>
+          Setiap hari Minggu pukul 09.00 WIB<br>(Siaran Langsung di Channel Youtube GKI Penginjil)</p>
+        </div>
+
+        <div class="col-md-4 col-sm-2">
+          <h3><b>KEBAKTIAN DOA<br>TENGAH MINGGU</b></h3>
+          <p>Setiap hari Kamis pukul 18.00 WIB<br>(Siaran Langsung di Channel Youtube GKI Penginjil)</p>
+        </div>
+        
+        <div class="col-md-4 col-sm-2">
+          <h3><b>PERSEKUTUAN<br>KOMISI USIA INDAH</b></h3>
+          <p>Setiap hari Selasa pukul 16.00 WIB</p>
+        </div>
+      </div>
+      <hr>
+      <div class="row">
+        <div class="col-md-4 col-sm-2">
+          <h3><b>PERSEKUTUAN<br>KOMISI PRIA</b></h3>
+          <p>Setiap hari Selasa pukul 18.00 WIB<br>(via ZOOM MEETING)</p>
+        </div>
+
+        <div class="col-md-4 col-sm-2">
+          <h3><b>PERSEKUTUAN<br>KOMISI WANITA</b></h3>
+          <p>Setiap hari Selasa pukul 18.00 WIB<br>(via ZOOM MEETING)</p>
+        </div>
+
+        <div class="col-md-4 col-sm-2">
+          <h3><b>PERSEKUTUAN<br>KOMISI PEMUDA</b></h3>
+          <p>Setiap hari Sabtu pukul 19.00 WIB</p>
+        </div>
+      </div>
+      <hr>
+      <div class="row">
+        <div class="col-md-4 col-sm-2">
+          <h3><b>PERSEKUTUAN<br>KOMISI REMAJA</b></h3>
+          <p>Setiap hari Sabtu pukul 16.00 WIB<br>(via ZOOM MEETING)</p>
+        </div>
+
+        <div class="col-md-4 col-sm-2">
+          <h3><b>PERSEKUTUAN<br>KOMISI ANAK</b></h3>
+          <p>Setiap hari Minggu pukul 10.30 WIB<br>(via ZOOM MEETING)</p>
+        </div>
+      </div>
+      <hr>
+
+      </div>    
+    </div>
+  </div>
+
+<?php
+  require 'include/footer.php';
+?>
